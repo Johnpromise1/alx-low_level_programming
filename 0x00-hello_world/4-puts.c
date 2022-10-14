a3949262473fe3d538cf7036e99f0c7636c544b1
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-13 19:42:04 -0700

    This is a task Use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - printing 
+ *
  * Return : Always 0 (Successful)
  */
 int main (void)
 {
-	puts("\"Prgramming is like building a multilingual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0);
+
+
 }
