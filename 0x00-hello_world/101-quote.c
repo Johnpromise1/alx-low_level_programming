d33403a46ac083740de650b2618549b7aca54198
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-14 05:32:10 -0700

    This is advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main-Entry
- * Return: 1
+ * main - Entry
+ * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
