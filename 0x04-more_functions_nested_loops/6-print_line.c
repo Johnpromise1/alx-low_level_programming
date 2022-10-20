1fb6e11ad7da6d98b721a65fa83c48055518fc09
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-20 12:58:25 -0700

    this is task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_line - prints a straight line using '-'
-* @n: The number of '-' to be printed
+* print_line - prints a straight line using '_'
+* @n: The number of '_' to be printed
 */
 
 void print_line(int n)
@@ -12,7 +12,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
-			_putchar('-');
+			_putchar('_');
 	}
 
 	_putchar('\n');
