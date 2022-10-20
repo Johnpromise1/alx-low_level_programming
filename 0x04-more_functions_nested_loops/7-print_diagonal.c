48ac83f9dfaa6441265851a547b9e09c7c8c8bfc
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-20 14:00:01 -0700

    this is task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_diagonal - draws a diagonal line 
-* @n: numbers of '\' to be printed 
+* print_diagonal - draws a diagonal line
+* @n: numbers of '\' to be printed
 */
 
 void print_diagonal(int n)
