1f0b36ff58d4b8eb315b1ea3347ceca3bae731ea
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-27 15:43:42 -0700

    TASK 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,12 +15,12 @@ void print_buffer(char *b, int size)
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-				printf("  ");
+				printf(" ");
 			else
 				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
 			{
-				printf("  ");
+				printf(" ");
 			}
 		}
 		for (index = 0; index < 10; index++)
