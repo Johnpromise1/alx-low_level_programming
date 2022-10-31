6f075cba3a59a1a878999f2399dc26bb70986f94
Johnpromise Ononuju <Johnpromiseononuju@gmail.com>
2022-10-31 11:57:27 -0700

    TASK 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+/*
+* File: 5-strstr.c
+* Auth: JP
+*/
+
+#include "main.h"
+
+/**
+* _strstr - Locates a substring
+* @haystack: The string to be searched
+* @needle: The subtring to be located
+* Return: if the substring is located - a pointer to the beginning
+*	if the subtring is not located - NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
